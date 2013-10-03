1180b35e3f01486a155924ecbc72f4d5ff16c88f
A. Qahar Mudzakkir <qahar@octave-tech.com>
2013-10-03 13:22:10 +0700

    add shortcodes extend per theme basis

---
+++ upload/catalog/view/theme/default/shortcodes.php
@@ -0,0 +1,19 @@
+<?php
+class themeShortcodes extends Shortcodes {
+   /* Quick Docs:
+    *  - Function name would be the shortcode tag.
+    *  - Use the same function name as in default shortcode to overwrite it.
+    */
+
+
+   /**
+    * Get theme info
+    *
+    * [themeinfo /]
+    */
+   function themeinfo() {
+      $data = 'This store use <b>' . $this->config->get('config_template') . '</b> theme.';
+      
+      return $data;
+   }
+}
