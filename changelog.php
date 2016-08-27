3d8d033c851106e13fea5f07c315c954bea261f1
A. Qahar Mudzakkir <qahar@octave-tech.com>
2016-08-28 01:15:49 +0700

    add changelog

---
+++ changelog.php
@@ -0,0 +1,42 @@
+=== Changelog
+* Fixed
+^ Improvements
++ New Feature
+~ Deprecated
+# New Feature - Beta
+
+
+=== ToDo
+- do_shortcodes on teaser modules then truncate
+- do_shortcodes on meta title and meta description
+
+#=== v2.x.x
+
+=== v2.2.0 - August 29, 2016
+^ OpenCart 2.3.0 compatibility
+
+=== v2.1.1 - June 5, 2016
+^ Improve [link_store] db query
+^ Add shortcodes support in search page
+
+=== v2.0.4 - June 5, 2016
+^ Add shortcodes support in search page
+
+=== v2.1.0 - March 6, 2016
+^ OpenCart 2.2.0 compatibility
+^ Replace vQmod with OcMod
+* Module slideshow load script
+~ Sysinfo
+
+=== v2.0.3 - September 1, 2015
+* Fixed magnific js and css loading repeatly
+
+=== v2.0.2 - July 10, 2015
+* Fixed load model for information
+
+=== v2.0.1 - June 9, 2015
+* OpenCart 2 customer group id
+
+=== v2.0.0 - April 27, 2015
++ OpenCart 2
+~ OpenCart 1.5.x
